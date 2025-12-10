111dcd112696bb8ac43d2c31d08f1c4f5489702a
Kamlesh Purohit <kamleshpurohit034@gmail.com>
2025-12-10 20:58:21 +0530

    New portfolio clean start

--- script.js
+++ script.js
@@ -2,24 +2,14 @@ var progressBars = document.querySelectorAll(".skill-progress > div");
 var skillsContainer = document.getElementById('skills-container');
 var animationDone = false;
 
- HEAD
 function initialiseBars() {
   for (var bar of progressBars) {
     bar.style.width = 0 + '%';
   }
-
-
-
-function initialiseBars() {
-    for (var bar of progressBars) {
-        bar.style.width = 0 + '%';
-    }
-
 }
 
 initialiseBars();
 
- HEAD
 function fillBars() {
   for (let bar of progressBars) {
     let currentWidth = 0;
@@ -46,7 +36,6 @@ function checkScroll() {
   }
 }
 
-}
 window.addEventListener("scroll", checkScroll);
 window.addEventListener("load", checkScroll);
 
@@ -75,43 +64,3 @@ function setActiveNav() {
 
 window.addEventListener("scroll", setActiveNav);
 window.addEventListener("load", setActiveNav);
-
-
-
-function fillBars() {
-
-    for (let bar of progressBars) {
-        let currentWidth = 0;
-        let interval = setInterval(function () {
-            let targetWidth = bar.getAttribute('data-bar-width');
-            if (currentWidth >= targetWidth) {
-                clearInterval(interval);
-                return;
-            }
-            currentWidth++;
-            bar.style.width = currentWidth + '%';
-        }, 5);
-    }
-}
-
-
-
-function checkScroll() {
-
-    var coordinates = skillsContainer.getBoundingClientRect();
-    if (!animationDone && coordinates.top <= window.innerHeight) {
-        animationDone = true;
-        fillBars();
-    } else if (coordinates.top > window.innerHeight) {
-        animationDone = false;
-        initialiseBars();
-    }
-}
-
-
-
-window.addEventListener("scroll", checkScroll);
-
-// This event fills the progress bars if they are displayed on the screen when the page is loaded.
-window.addEventListener("load", checkScroll);
-
